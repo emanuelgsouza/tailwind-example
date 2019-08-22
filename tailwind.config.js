b1c222e0d72cf58ad2a42cf4dd665be02a818b0e
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-21 22:58:08 -0300

    config a primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        // https://tailwindcss.com/docs/customizing-colors#gray
+        // cor de gray-800
+        primary: '#2d3748'
+      }
+    }
   },
   variants: {},
   plugins: []
